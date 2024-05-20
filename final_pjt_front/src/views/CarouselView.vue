db1176a0bcc3a59691978c5d3104b301d6ba6330
JiEung2 <181757@jnu.ac.kr>
2024-05-20 16:07:47 +0900

    Carousel 추가

---
+++ final_pjt_front/src/views/CarouselView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item" :class="{ active: index === 0 }" v-for="(movie, index) in movieStore.popularMovies" :key="index">
+        <img v-if="movie.poster_path" :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" class="d-block carousel-img" :alt="movie.title">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { useMovieStore } from '@/stores/counter';
+import { onMounted } from 'vue';
+
+const movieStore = useMovieStore();
+
+onMounted(() => {
+  movieStore.getPopularMovieList();
+});
+</script>
+
+<style scoped>
+.carousel-inner {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 500px; /* Adjust the height as needed */
+  margin-right: 50%;
+}
+
+.carousel-item {
+  height: 100%; /* Match the height of the carousel-inner */
+}
+
+.carousel-img {
+  max-height: 100%;
+  max-width: 100%;
+}
+</style>
