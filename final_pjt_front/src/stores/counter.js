bd0c318bb5791cbc739272b4f0393ff96965dd12
JiEung2 <181757@jnu.ac.kr>
2024-05-20 09:05:05 +0900

    recreate final_pjt_front

--- final_pjt_front/src/stores/counter.js
+++ final_pjt_front/src/stores/counter.js
@@ -1,12 +1,29 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useMovieStore = defineStore('movie', () => {
+  const popularMovies = ref([])
+  const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN
+  
+  const getPopularMovieList = function() {
+    if (popularMovies.value.length == 0){
+      axios({
+        method: 'get',
+        url: 'https://api.themoviedb.org/3/movie/popular?language=ko&page=1',
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${TMDB_TOKEN}`
+        }
+      }).then((response) => {
+        // console.log(response.data);
+        popularMovies.value = response.data.results
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
   }
-
-  return { count, doubleCount, increment }
+  
+  return { popularMovies, getPopularMovieList }
 })
+
