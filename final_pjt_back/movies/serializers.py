4168dbbbe6968352a4f4f29ee182725d462865ca
JiEung2 <181757@jnu.ac.kr>
2024-05-21 14:57:34 +0900

    검색 페이지, 최신 예고편, 로그인, 회원가입 완료

--- final_pjt_back/movies/serializers.py
+++ final_pjt_back/movies/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Movie, Comment
+from .models import Movie, Comment, Genre
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +11,8 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = '__all__'
         read_only_fields = ('movie', 'created_at', 'user',)
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
--- final_pjt_back/movies/urls.py
+++ final_pjt_back/movies/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from . import views
 
+app_name='movies'
 urlpatterns = [
   path('getMovies/', views.getMovieData),
   path('getNowPlayingMovies/', views.getNowPlayingMovieData),
   path('getGenres/', views.getGenreData),
   path('movies/popular/', views.popular, name='popular'),
   path('movies/late_release/', views.late_release, name='late_release'),
+  path('movies/<str:movie_name>/', views.search_movie, name='search_movie'),
   path('movies/<int:movie_id>/', views.comment_list),
+  path('genres/<int:genre_id>/', views.search_genre),
   path('movies/<int:movie_id>/<int:comment_id>/', views.comment_detail),
   path('movies/<int:movie_id>/comments/create/', views.comment_create)
 ]
